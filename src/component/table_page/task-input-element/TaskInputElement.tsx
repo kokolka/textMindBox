f3746f899dab5f67469953f57557d4658a9dfd02
Kirill Balachoncev <kirill-i_98@mail.ru>
2023-09-06 22:15:17 +0700

    добавил валидацию в форму

--- src/component/table_page/task-input-element/TaskInputElement.tsx
+++ src/component/table_page/task-input-element/TaskInputElement.tsx
@@ -1,31 +1,62 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
-import { Formik, Form, Field } from 'formik';
+import { Formik, Form, Field, FormikErrors, FormikProps, withFormik } from 'formik';
 import { addTask } from '../../../store/taskAC';
 
 interface MyFormValues {
   newTask: string;
 }
 
-export const TaskInputElement: React.FC<any> = () => {
-  const initialValues: MyFormValues = { newTask: '' };
+const InnerForm = (props: FormikProps<MyFormValues>) => {
+  const { touched, errors, isSubmitting } = props;
 
+  return (
+    <Form>
+      <div>
+        <Field type="text" id="newTask" name="newTask" placeholder="Введите новое задание" />
+        {touched.newTask && errors.newTask && <div>{errors.newTask}</div>}
+      </div>
+
+      <button type="submit" disabled={isSubmitting}>
+        Add task
+      </button>
+    </Form>
+  );
+};
+
+interface InitialFormValues {
+  setData: (text: string) => void;
+}
+
+const MyForm = withFormik<InitialFormValues, MyFormValues>({
+  mapPropsToValues: (props) => {
+    return {
+      newTask: '',
+    };
+  },
+  validate: (value: MyFormValues) => {
+    let errors: FormikErrors<MyFormValues> = {};
+    if (value.newTask.length < 1) {
+      errors.newTask = 'No empty';
+    }
+    return errors;
+  },
+  handleSubmit: (value, formikBag) => {
+    formikBag.props.setData(value.newTask);
+    formikBag.setSubmitting(false);
+    value.newTask = '';
+  },
+})(InnerForm);
+
+export const TaskInputElement: React.FC<any> = () => {
   const dispatch = useDispatch();
+  const setData = (text: string) => {
+    dispatch(addTask(text));
+  };
 
   return (
     <div>
-      <Formik
-        initialValues={initialValues}
-        onSubmit={(values, actions) => {
-          dispatch(addTask(values.newTask));
-          actions.setSubmitting(false);
-          values.newTask = '';
-        }}>
-        <Form>
-          <Field type="text" id="newTask" name="newTask" placeholder="Введите новое задание" />
-          <button type="submit">Добавить задачу</button>
-        </Form>
-      </Formik>
+      <MyForm setData={setData} />
     </div>
   );
 };
